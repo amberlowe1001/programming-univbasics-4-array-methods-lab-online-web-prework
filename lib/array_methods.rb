80d54d0f9493b2447a811461223df8033a84e6b4
lite-post-0822 <amberlowe40@gmail.com>
2019-07-11 04:49:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,20 +11,20 @@ def methods =["wow", "I","am","really","learning","arrays"]
   end
 end
 
-def meth =["wow", "I","am","really,"learning","arrays"]
+def methods =["wow", "I","am","really,"learning","arrays"]
 
 end
 
-def using_first =["wow", "I","am","really,"learning","arrays"]
+def methods =["wow", "I","am","really,"learning","arrays"]
   using_first.first
 
 end
 
-def using_last =["wow", "I","am","really,"learning","arrays"]
+def methods =["wow", "I","am","really,"learning","arrays"]
 using_last.last
 
 end
 
-def using_size =["wow", "I","am","really,"learning","arrays"]
+def methods =["wow", "I","am","really,"learning","arrays"]
 
 end
