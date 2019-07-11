bd3e7484f5dee4fdcd0a360952920ef8b83da140
lite-post-0822 <amberlowe40@gmail.com>
2019-07-11 05:09:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,38 +1,35 @@
-def methods =["wow","I","am","really","learning","arrays"]
+def methods_include =["wow","I","am","really","learning","arrays"]
  binding.pry
-  methods.include?("wow")
-  methods.include?("Tardis")
-
-  end
-end
-
-def methods =["wow","I","am","really","learning","arrays"]
-  methods.sort
+  methods_include.include?("wow")
+  methods_include.include?("Tardis")
   
+end
+
+def methods_sort =["wow","I","am","really","learning","arrays"]
+  methods_sort.sort
+  
+  
+end
+
+def methods_reverse =["wow","I","am","really","learning","arrays"]
+methods_reverse.reverse
+
+end
+
+
+def methods_first =["wow","I","am","really","learning","arrays"]
+  methods_first.first
+
+end
+
+def methods_last =["wow", "I","am","really","learning","arrays"]
+methods_last.last
+
   end
 end
 
-def methods =["wow","I","am","really","learning","arrays"]
-methods.reverse
-
-  end
-end
-
-
-def methods =["wow","I","am","really","learning","arrays"]
-  methods.first
-
- end
-end
-
-def methods =["wow", "I","am","really","learning","arrays"]
-methods.last
-
-  end
-end
-
-def methods =["wow", "I","am","really","learning","arrays"]
-  methods.size
+def methods_size =["wow", "I","am","really","learning","arrays"]
+  methods_size.size
 
   end
 end
